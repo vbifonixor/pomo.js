76b8ecc540954d63e292277e9347712dc3476296
Mikhail Kononenko <me@vbifonixor.ru>
2015-10-31 12:05:38 +0300

    Basic layout added

--- src/assets/js/global.js
+++ src/assets/js/global.js
@@ -3,5 +3,7 @@
 (function ($) {
 	'use strict';
 
-
+	VMasker(document.querySelector(".main-timer")).maskPattern('99:99');
+	VMasker(document.querySelector(".short-break")).maskPattern('99:99');
+	VMasker(document.querySelector(".long-break")).maskPattern('99:99');
 })(jQuery);
