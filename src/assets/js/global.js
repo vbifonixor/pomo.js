b99eaf90f101366198dd3c0ebb0ab01c305a42e3
Mikhail Kononenko <me@vbifonixor.ru>
2015-11-12 02:08:21 +0300

    Countdown works now. Not without some bugs

--- src/assets/js/global.js
+++ src/assets/js/global.js
@@ -16,9 +16,11 @@
 				 time: {},
 				 settings: {}, 
 				 dom: {},
+				 seq: ["main", "short", "main", "short", "main", "short", "main", "long"],
 			   };
 
-	// Helper functions
+	// Helper functions and variables
+	pomo.helpers.isGoing = 0;
 	pomo.helpers.timeFromText = function(text) {
 		return text.split(":").map( function (timeStr) { return parseInt(timeStr); } );
 	}
@@ -53,6 +55,34 @@
 		return !pomo.helpers.arraysAreEqual(pomo.time.mainBefore.length ? pomo.time.mainBefore : pomo.time.main, pomo.helpers.timeFromText(pomo.dom.face.text()));
 	}
 
+	pomo.m.beep = function() {
+		return true;
+	}
+
+	pomo.m.start = function() {
+		if (pomo.helpers.arraysAreEqual(pomo.currentTime.time.data, [])) {
+			pomo.currentTime.cycle = "main";
+			pomo.currentTime.time.data = pomo.time.main;
+		};
+		pomo.helpers.interval = setInterval(function () {
+			// Countdown goes here
+			pomo.dom.face.text(pomo.helpers.textFromTime(pomo.currentTime.time.data));
+			if(!pomo.currentTime.time.decrease()) {
+				pomo.m.beep();
+				pomo.currentTime.seqNo += 1;
+				pomo.currentTime.seqNo %= 8;
+				pomo.currentTime.cycle = pomo.seq[pomo.currentTime.seqNo];
+				pomo.currentTime.data = pomo.time[pomo.currentTime.cycle];
+			}
+		}, 1000)
+		return true;
+	}
+	pomo.m.pause = function() {
+		clearInterval(pomo.helpers.interval);
+		return true;
+	}
+
+
 	// jQuery DOM objects
 	pomo.dom = {
 		face: $("[pomo-face]"), // The clock face
@@ -78,6 +108,27 @@
 		short: pomo.helpers.timeFromInput(pomo.dom.timeInputs.short),
 		long: pomo.helpers.timeFromInput(pomo.dom.timeInputs.long) 
 	};
+	// An object for saving current timer time
+	pomo.currentTime = {
+		cycle: "main", // default value, changes when going through next cycle
+		seqNo: 0,
+		time: {
+			data: [],
+			decrease: function(){
+				if (this.data[0] === 0 && this.data[1] === 0) {
+					return false;
+				}
+				if (this.data[1] === 0) {
+					this.data[0] = this.data[0] - 1;
+					this.data[1] = 59;
+				}
+				else {
+					this.data[1] = this.data[1] - 1;
+				}
+				return true;
+			}
+		}
+	}
 	// Nonstop and mute settings
 	pomo.settings = {
 		nonstop: pomo.dom.settings.nonstop[0].checked,
@@ -101,8 +152,19 @@
 		if (!pomo.m.isStarted()){
 			pomo.dom.face.text(pomo.helpers.textFromTime(pomo.time.main));
 		}
-	})
+	});
 
+	// Button events
+	pomo.dom.startPauseBtn.on('click', function() {
+		if (pomo.dom.startPauseBtn.attr("pomo-btn") === "start") {
+			pomo.dom.startPauseBtn.attr("pomo-btn", "pause").text("Pause");
+			pomo.m.start();
+		}
+		else {
+			pomo.dom.startPauseBtn.attr("pomo-btn", "start").text("Start");
+			pomo.m.pause();
+		}
+	});
 
 	console.log(pomo); //d
 	exports.pomo = pomo;
